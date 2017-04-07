04e5891376ed969ce002888a0366cd0bea60975f
VasamIO <venkatesh.vasam@cloudio.io>
2017-04-07 21:57:22 +0530

    Home Page design

--- src/pages/AddVehicle/AddVehicle.ts
+++ src/pages/AddVehicle/AddVehicle.ts
@@ -6,21 +6,42 @@ import { NavController , ToastController, Events} from 'ionic-angular';
 })
 export class AddVehicle {
   
+  vehicles: any = {};
+  initVehicles() {
+    this.vehicles['recent'] = [];
+    this.vehicles['favorite'] = [
+      {
+        dp : "../assets/images/cab1.jpg",
+        title : "SRS Cab-1",
+        desc : "SRS Travels, Hyderabad",
+      },
+       {
+        dp : "../assets/images/cab2.jpg",
+        title : "SRS Cab-2",
+        desc : "SRS Travels, Hyderabad"
+      },
+       {
+        dp : "../assets/images/cab3.jpg",
+        title : "SRS Cab-33",
+        desc : "SRS Travels, Hyderabad"
+      }
+
+    ];
+    this.vehicles.length = 2;
+  }
   constructor(public navCtrl: NavController, public toastCtrl : ToastController,
   public event: Events) {}
   qrClick(){
-     let toast = this.toastCtrl.create({
-      message: 'This feature is not implemented yet',
-      duration: 3000
-    });
-    toast.present();
+    this.addVehicle();
+    //  let toast = this.toastCtrl.create({
+    //   message: 'This feature is not implemented yet',
+    //   duration: 3000
+    // });
+    // toast.present();
   }
   addVehicle(){
-    let data = {
-      id : "test",
-      name : "test_name"
-    };
-    this.event.publish("add:vehicle", data);
+    this.initVehicles();
+    this.event.publish("add:vehicle", this.vehicles);
     this.navCtrl.pop();
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,15 +8,40 @@ import { AddVehicle } from '../AddVehicle/AddVehicle';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  
-  constructor(public navCtrl: NavController, public events : Events) {}
-  
-  ionViewDidLoad(){
-   this.events.subscribe("add:vehicle", (data) => {
-   });
+
+  vehicles: any = [];
+
+  constructor(public navCtrl: NavController, public events: Events) {}
+ 
+  getVehicleCount = function () {
+    this.vehicles.length;
   }
-  
-  addVehicle(){
+
+  ionViewDidLoad() {
+    this.events.subscribe("add:vehicle", (vehicles) => {
+      this.vehicles = vehicles;
+    });
+  }
+  addVehicle() {
     this.navCtrl.push(AddVehicle);
+  }
+
+  selectVehicle(){
+    
+  }
+
+  searchVehicel(ev: any) {
+    // Reset items back to all of the items
+    // this.initializeItems();
+
+    // // set val to the value of the searchbar
+    // let val = ev.target.value;
+
+    // // if the value is an empty string don't filter the items
+    // if (val && val.trim() != '') {
+    //   this.items = this.items.filter((item) => {
+    //     return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+    //   })
+    // }
   }
 }
